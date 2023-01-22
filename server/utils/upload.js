89cc26748551a8567ae2d7d1b23f5d8d4de3d43b
Shobhitpatel19 <shobhitp49@gmail.com>
2023-01-22 05:50:24 +0530

    push

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,11 +1,12 @@
 import multer from 'multer';
 import { GridFsStorage } from 'multer-gridfs-storage';
-// import { dotenv } from '../../server/'
+import dotenv from 'dotenv';
 
-// dotenv.config();
-
+dotenv.config();
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
 const storage = new GridFsStorage({
-    url: `mongodb://shobhitpatel:shobhitpatel@ac-prcunar-shard-00-00.ozabtb6.mongodb.net:27017,ac-prcunar-shard-00-01.ozabtb6.mongodb.net:27017,ac-prcunar-shard-00-02.ozabtb6.mongodb.net:27017/?ssl=true&replicaSet=atlas-ewr5i3-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    url: `mongodb://${username}:${password}@ac-prcunar-shard-00-00.ozabtb6.mongodb.net:27017,ac-prcunar-shard-00-01.ozabtb6.mongodb.net:27017,ac-prcunar-shard-00-02.ozabtb6.mongodb.net:27017/?ssl=true&replicaSet=atlas-ewr5i3-shard-0&authSource=admin&retryWrites=true&w=majority`,
     options: { useNewUrlParser: true },
     file: (request, file) => {
         const match = ["image/png", "image/jpg"];
